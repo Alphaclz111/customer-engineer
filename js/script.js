3c8e189da4f88c35af18794ea6b3ec1c2bab33f9
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-16 12:55:00 +0600

    show name email data

--- js/script.js
+++ js/script.js
@@ -15,6 +15,12 @@ function handleSubmit(event) {
   // Do something with the name, email, and message data, such as sending it to a server or displaying it on the page
   console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
 
+  // Display the submitted data in the display container
+  const nameDisplay = document.getElementById("nameDisplay");
+  const emailDisplay = document.getElementById("emailDisplay");
+  nameDisplay.innerText = `Name: ${name}`;
+  emailDisplay.innerText = `Email: ${email}`;
+
   (function (a, b, c, d, e, f, g) { e['ire_o'] = c; e[c] = e[c] || function () { (e[c].a = e[c].a || []).push(arguments) }; f = d.createElement(b); g = d.getElementsByTagName(b)[0]; f.async = 1; f.src = a; g.parentNode.insertBefore(f, g); })('https://utt.impactcdn.com/A2788006-de3d-4011-a57c-c4737e79de0365535.js', 'script', 'ire', document, window); ire('identify', { customerid: name, customeremail: email });
 
   // Reset the form after submission
