f1438f15836a60074511cf546189922386983f7b
Alphaclz111 <127713750+Alphaclz111@users.noreply.github.com>
2023-03-19 17:43:41 +0600

    gtm change

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ function handleSubmit(event) {
     }); var f = d.getElementsByTagName(s)[0],
       j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
         'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-  })(window, document, 'script', 'dataLayer', 'GTM-WTH2XZ9');
+  })(window, document, 'script', 'dataLayer', 'GTM-TJM3456');
 
   // Reset the form after submission
   event.target.reset();
