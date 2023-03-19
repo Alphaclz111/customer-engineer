e52f2d3014de7d7e641d8a21e911496057c0c5cb
Alphaclz111 <127713750+Alphaclz111@users.noreply.github.com>
2023-03-19 17:20:43 +0600

    link update

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ function handleSubmit(event) {
         new Date().getTime(), event: 'gtm.js'
     }); var f = d.getElementsByTagName(s)[0],
       j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-        'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+        'https://utt.impactcdn.com/A2788006-de3d-4011-a57c-c4737e79de0365535.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
   })(window, document, 'script', 'dataLayer', 'GTM-WTH2XZ9');
 
   // Reset the form after submission
